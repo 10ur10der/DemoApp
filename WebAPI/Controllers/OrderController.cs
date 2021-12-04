a8ecd689229536216a367eaf58e7cf3962d1def4
Onur Önder <10ur10der@gmail.com>
2021-12-04 14:40:02 +0300

    RabbitMq proje eklendi.

--- WebAPI/Controllers/OrderController.cs
+++ WebAPI/Controllers/OrderController.cs
@@ -4,11 +4,16 @@ using Entities;
 using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text;
 
 namespace WebAPI.Controllers
 {
@@ -21,13 +26,28 @@ namespace WebAPI.Controllers
         private IBillingService _billingService;
         private IProductService _productService;
         private readonly ILogger<OrderController> _logger;
+        private readonly IConfiguration _configuration;
+        private IConnection _connection;
+        private readonly ConnectionFactory factory;
 
-        public OrderController(IOrderService orderService, IBillingService billingService, IProductService productService, ILogger<OrderController> logger)
+        public OrderController(IOrderService orderService, IBillingService billingService, IProductService productService, ILogger<OrderController> logger, IConfiguration configuration)
         {
             _orderService = orderService;
             _billingService = billingService;
             _productService = productService;
             _logger = logger;
+            _configuration = configuration;
+            //_connection = connection;
+
+            factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+
+            };
+
+            _connection = factory.CreateConnection();
         }
 
         [HttpPost(template: "add")]
@@ -44,16 +64,50 @@ namespace WebAPI.Controllers
                 {
                     prod.Data.Stock -= order.Quantity;
                     _productService.Update(prod.Data);
-                    //Billing billing = new Billing();
-                    //_billingService.Create(billing);
+                    SendBilling(order);
                     _logger.LogInformation(result.Message);
                     return Ok(result.Message);
                 }
                 _logger.LogInformation(result.Message);
                 return BadRequest(result.Message);
             }
-            return BadRequest();
-        
+            return BadRequest("Stokta Ürün bulunamadı");
+
         }
+
+
+        private void SendBilling(Order order)
+        {
+            Billing billing = new Billing();
+            billing.Order = order;
+            billing.ItemPrice = 100;
+            billing.JsonTaxes = "{\"Key\":1,\"Value\":20}";
+            billing.JsonDiscounts = "{\"Key\":2,\"Value\":5}";
+            billing.TotalCost = 50;
+            _billingService.Create(billing);
+
+            using (var channel = this._connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue:"customer",
+                    durable:false,
+                    exclusive:false,
+                    autoDelete:false,
+                    arguments:null
+                    );
+
+                var customerBilling = System.Text.Json.JsonSerializer.Serialize(billing);
+                var body = Encoding.UTF8.GetBytes(customerBilling);
+
+                channel.BasicPublish(
+                    exchange:"",
+                    routingKey:"customer",
+                    basicProperties:null,
+                    body:body
+                    );
+            }
+        }
+       
+    
     }
 }
